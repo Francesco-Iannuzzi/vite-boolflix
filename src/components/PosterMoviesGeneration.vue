3f772906343e9253d1d4d02512bab6f15b5c819f
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-04-20 15:55:22 +0200

    add empty poster

--- src/components/PosterMoviesGeneration.vue
+++ src/components/PosterMoviesGeneration.vue
@@ -12,16 +12,21 @@ export default {
     },
     methods: {
         generationPosterMovie(movie) {
-            let posterMovie = state.API_URL_poster + movie.poster_path
-            console.log(posterMovie);
-            return posterMovie
+            if (movie.poster_path !== null) {
+                let posterMovie = state.API_URL_poster + movie.poster_path
+                console.log(posterMovie);
+                return posterMovie
+            } else {
+                let posterMovieEmpty = 'https://media.istockphoto.com/id/1216251206/vector/no-image-available-icon.jpg?s=170667a&w=0&k=20&c=N-XIIeLlhUpm2ZO2uGls-pcVsZ2FTwTxZepwZe4DuE4='
+                return posterMovieEmpty
+            }
         }
     }
 }
 </script>
 
 <template>
-    <img :src="generationPosterMovie(elementPosterMovie)" alt="">
+    <img width="92" height="138" :src="generationPosterMovie(elementPosterMovie)" alt="">
 </template>
 
 
--- src/components/PosterSeriesGeneration.vue
+++ src/components/PosterSeriesGeneration.vue
@@ -12,16 +12,21 @@ export default {
     },
     methods: {
         generationPosterSerie(serie) {
-            let posterSerie = state.API_URL_poster + serie.poster_path
-            console.log(posterSerie);
-            return posterSerie
+            if (serie.poster_path !== null) {
+                let posterSerie = state.API_URL_poster + serie.poster_path
+                console.log(posterSerie);
+                return posterSerie
+            } else {
+                let posterSerieEmpty = 'https://media.istockphoto.com/id/1216251206/vector/no-image-available-icon.jpg?s=170667a&w=0&k=20&c=N-XIIeLlhUpm2ZO2uGls-pcVsZ2FTwTxZepwZe4DuE4='
+                return posterSerieEmpty
+            }
         }
     }
 }
 </script>
 
 <template>
-    <img :src="generationPosterSerie(elementPosterSerie)" alt="">
+    <img width="92" height="138" :src="generationPosterSerie(elementPosterSerie)" alt="">
 </template>
 
 
